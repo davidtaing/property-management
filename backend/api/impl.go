e3dd878dfaa061760f1baa417da041f68a5cf626
David Taing <dave@teeang.net>
2025-05-16 11:43:30 +1000

    Implement CRUD operations for Landlords in the API
    - Added functionality to list, create, update, archive, and retrieve landlords.
    - Integrated JSON encoding/decoding for request and response handling.
    - Enhanced error handling for database operations.
    - Updated HTTP response statuses and content types accordingly.

--- backend/api/impl.go
+++ backend/api/impl.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"context"
+	"encoding/json"
 	"net/http"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -20,29 +22,163 @@ func NewServer(dbpool *pgxpool.Pool) *Server {
 }
 
 func (s *Server) LandlordsList(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
-	w.Write([]byte("Not yet implemented"))
+	landlords := []Landlord{}
+
+	rows, err := s.dbpool.Query(context.Background(), "SELECT * FROM landlords")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var landlord Landlord
+		err := rows.Scan(&landlord.Id, &landlord.Name, &landlord.Email, &landlord.Mobile, &landlord.Phone, &landlord.IsArchived)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}	
+		landlords = append(landlords, landlord)
+	}
+
+	if err := rows.Err(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(LandlordList{Items: landlords})
 }
 
 func (s *Server) LandlordsCreate(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
-	w.Write([]byte("Not yet implemented"))
+	var payload Landlord
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	rows, err := s.dbpool.Query(
+		context.Background(),
+		`INSERT INTO landlords (
+			name,
+			email,
+			mobile,
+			phone
+		) VALUES (
+			$1,
+			$2,
+			$3,
+			$4
+		) RETURNING *`,
+		payload.Name,
+		payload.Email,
+		payload.Mobile,
+		payload.Phone,
+	)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var createdLandlord Landlord
+
+	if rows.Next() {
+		err = rows.Scan(&createdLandlord.Id, &createdLandlord.Name, &createdLandlord.Email, &createdLandlord.Mobile, &createdLandlord.Phone, &createdLandlord.IsArchived)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(createdLandlord)
 	
 }
 
 func (s *Server) LandlordsArchive(w http.ResponseWriter, r *http.Request, id string) {
-	w.WriteHeader(http.StatusNotImplemented)
-	w.Write([]byte("Not yet implemented"))
+	var archivedLandlord Landlord
+
+	
+	rows, err := s.dbpool.Exec(context.Background(), "UPDATE landlords SET is_archived = NOW() WHERE id = $1", id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if rows.RowsAffected() == 0 {
+		http.Error(w, "Landlord not found", http.StatusNotFound)
+		return
+	}
+
+	err = s.dbpool.QueryRow(context.Background(), "SELECT * FROM landlords WHERE id = $1", id).Scan(&archivedLandlord.Id, &archivedLandlord.Name, &archivedLandlord.Email, &archivedLandlord.Mobile, &archivedLandlord.Phone, &archivedLandlord.IsArchived)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(archivedLandlord)
 }
 
 func (s *Server) LandlordsGet(w http.ResponseWriter, r *http.Request, id string) {
-	w.WriteHeader(http.StatusNotImplemented)
-	w.Write([]byte("Not yet implemented"))
+	var landlord Landlord
+
+	err := s.dbpool.QueryRow(context.Background(), "SELECT * FROM landlords WHERE id = $1", id).Scan(&landlord.Id, &landlord.Name, &landlord.Email, &landlord.Mobile, &landlord.Phone, &landlord.IsArchived)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(landlord)
 }
 
 func (s *Server) LandlordsUpdate(w http.ResponseWriter, r *http.Request, id string) {
-	w.WriteHeader(http.StatusNotImplemented)
-	w.Write([]byte("Not yet implemented"))
+	var payload Landlord
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	rows, err := s.dbpool.Query(
+		context.Background(),
+		`UPDATE landlords SET
+			name = $1,
+			email = $2,
+			mobile = $3,
+			phone = $4
+		WHERE id = $5
+		RETURNING *`,
+		payload.Name,
+		payload.Email,
+		payload.Mobile,
+		payload.Phone,
+		id,
+	)
+
+	var updatedLandlord Landlord
+
+	if rows.Next() {
+		err = rows.Scan(&updatedLandlord.Id, &updatedLandlord.Name, &updatedLandlord.Email, &updatedLandlord.Mobile, &updatedLandlord.Phone, &updatedLandlord.IsArchived)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(updatedLandlord)
 }
 
 func (s *Server) PropertiesList(w http.ResponseWriter, r *http.Request) {
