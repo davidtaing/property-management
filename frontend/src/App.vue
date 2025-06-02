1f8b7392901a2a2efe33487467cc5fb1fb1428e7
David Taing <dave@teeang.net>
2025-06-02 13:14:54 +1000

    feat: initialize Vue 3 + TypeScript + Vite project
    - Added initial project structure for a Vue 3 application using TypeScript and Vite.
    - Included essential files such as `index.html`, `package.json`, and configuration files for TypeScript and Vite.
    - Created a basic Vue component structure with `App.vue` and `HelloWorld.vue`, along with a sample token retrieval feature using Clerk for authentication.
    - Set up a `.gitignore` file to exclude unnecessary files and directories.
    - Added a README to guide users on project setup and usage.it

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { SignedIn, SignedOut, SignInButton, UserButton, useAuth } from '@clerk/vue'
+
+const { getToken } = useAuth()
+</script>
+
+<template>
+  <header>
+    <SignedOut>
+      <SignInButton />
+    </SignedOut>
+    <SignedIn>
+      <UserButton />
+
+      <button @click="async () => {
+          const token = await getToken()
+          console.log('Token:', token)
+      }">Get Token</button>
+    </SignedIn>
+  </header>
+</template>
